bc2d9d3f1b7373b001c0fe24d8aa8e58b2f7ba15
Vikrantkaramore <vikrantkaramore@gmail.com>
2026-05-31 23:10:49 +0530

    Add Vercel backend API routes and backend deployment support

---
+++ api/health.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  res.status(200).json({ status: 'Server is running!' });
+};
